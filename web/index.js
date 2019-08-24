2ee076ecd50d352b3b101f923ae5929c4120391c
Aurelio Gualda <aurelio.gualda@intelie.com.br>
2019-08-24 17:39:06 -0300

    test react

---
+++ web/index.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+class App extends Component {
+
+	render() {
+		return <div>
+                test react
+        </div>
+	}
+}
+
+ReactDOM.render(<App/>,document.getElementById('react'))
